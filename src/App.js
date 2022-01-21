7a4190f4062967b3d2532db63ea39fb904d90bc8
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-01-21 15:55:19 +0300

    fetch data from profile and repos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,47 @@ import { HOME_SCREEN, STARED_REPOS_SCREEN } from "./constants/navigations";
 import { HomeScreen, StaredReposScreen } from "./screens";
 
 const App = () => {
+  function fetchGithubProfil(username) {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const name = data.name;
+        const location = data.location;
+        const company = data.company;
+        const avatar = data.avatar_url;
+        const repos = data.public_repos;
+        const followers = data.followers;
+        const following = data.following;
+        console.log(
+          name,
+          location,
+          company,
+          avatar,
+          repos,
+          followers,
+          following
+        );
+      });
+  }
+
+  function fetchGithubRepository(username) {
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data.forEach((repo) => {
+          const name = repo.name;
+          const createdAt = repo.created_at;
+          const url = repo.url;
+          const languages = repo.language;
+          const visibility = repo.visibility;
+          console.log(name, createdAt, url, languages, visibility);
+        });
+      });
+  }
+
+  fetchGithubProfil("Sevil-h");
+  fetchGithubRepository("Sevil-h");
   return (
     <Router>
       <div>
