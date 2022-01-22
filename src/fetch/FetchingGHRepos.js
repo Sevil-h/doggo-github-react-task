b15c54a2ab036178c707c4e866d6a3b463d7908f
Sabrie Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-01-22 14:59:59 +0300

    date: sorted and formatted

--- src/fetch/FetchingGHRepos.js
+++ src/fetch/FetchingGHRepos.js
@@ -10,14 +10,27 @@ function FetchingGHRepos() {
     axios
       .get(`https://api.github.com/users/Sevil-h/repos`)
       .then((res) => {
-        const data = res.data;
         // console.log(data);
+
+        // sorting repositories depend on the created time
+        const data = res.data.sort((a, b) =>
+          new Date(a.created) < new Date(b.created_at) ? 1 : -1
+        );
         setRepos(data);
       })
       .catch((err) => {
         // console.log(err);
       });
   }, []);
+
+  // Date has been formetted as a August 10, 2021
+  repos.forEach((repo) => {
+    repo.created_at = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    }).format(new Date(repo.created_at));
+  });
 
   return (
     <div>
